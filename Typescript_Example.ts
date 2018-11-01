3eec9a856e607b787a68b7ce9db73297b3cd24d6
lx4r <hallo@l3r.de>
2018-11-01 13:01:49 +0100

    kleines Beispiel hinzugefügt

---
+++ Typescript_Example.ts
@@ -0,0 +1,37 @@
+enum ShopType {
+    Food,
+    Clothing,
+    Tech,
+}
+
+interface Shop {
+    shopType: ShopType;
+    calculateDiscountedPrice(normalPrice): number;
+}
+
+class DonutShop implements Shop {
+    private superSecretIngredient: string;
+    public currentDiscount: number;
+    public shopType: ShopType;
+    constructor(){
+        this.superSecretIngredient = "sugar";
+        this.currentDiscount = 0.1;
+        this.shopType = ShopType.Food;
+    }
+    calculateDiscountedPrice(normalPrice: number): number{
+        return normalPrice * this.currentDiscount;
+    }
+}
+
+class PCStore implements Shop {
+    private rootPassword: string;
+    public shopType: ShopType;
+    constructor(){
+        this.rootPassword = "PASSWORD";
+        this.shopType = ShopType.Tech;
+    }
+    calculateDiscountedPrice(normalPrice: number): number{
+        // NO DISCOUNTS!
+        return normalPrice;
+    }
+}
